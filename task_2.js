5feead696e1da411492fcd0295a581ea831e79e0
KonTroAl <troshenkin.k@yandex.ru>
2020-11-30 21:03:17 +0500

    task 2 completed

---
+++ task_2.js
@@ -0,0 +1,28 @@
+// 2. Продолжить работу с интернет-магазином:
+// В прошлом домашнем задании вы реализовали корзину на базе массивов. Какими объектами можно заменить их элементы?
+// Реализуйте такие объекты.
+// Перенести функционал подсчета корзины на объектно-ориентированную базу.
+
+
+let basket = {
+    fist: { name: 'PS', cost: 47000, num: 3 },
+    second: { name: 'Xbox', cost: 46000, num: 4 },
+    third: { name: 'PC', cost: 110000, num: 2 },
+    countBasketPrice() {
+        let a = 0;
+        let res = 0;
+        is_num = true;
+        for (let i in basket) {
+            a = (basket[i]["cost"] * basket[i]["num"]);
+            if (isNaN(a)) {
+                is_num = false;
+                break;
+            }
+            res = res + a;
+        }
+        console.log(res);
+    }
+}
+
+basket.countBasketPrice();
+
